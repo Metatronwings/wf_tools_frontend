6e084d303f539d1683dea5fd0ef9d13ec74ac955
DESKTOP-MQNHQB3\admin <metatronwings@foxmail.com>
2022-04-10 11:38:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { NCard } from 'naive-ui'
+</script>
+
+<template>
+  <n-card title="带封面的卡片">
+    <template #cover>
+      <img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
+    </template>
+    卡片内容
+  </n-card>
+</template>
+
+<style>
+.n-card {
+  max-width: 300px;
+}
+</style>
